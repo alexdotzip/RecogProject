559adf8fbd7290eab96475ad85b0267928f8da35
alexdotexe <ajpurpora@gmail.com>
2020-10-31 12:15:53 -0400

    Number Identifier
    Added logic to identify numbers on a basic level

--- image-identifier/imagerec.py
+++ image-identifier/imagerec.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from functools import reduce
+from collections import Counter
 
 
 def createExamples():
@@ -53,32 +54,38 @@ def threshold(imageArray):
 
     return newAr
 
-createExamples()
-'''i = Image.open('images/numbers/0.1.png')
-iar = np.array(i)
 
-i2 = Image.open('images/numbers/y0.4.png')
-iar2 = np.array(i2)
+def whatNumIsThis(filePath):
 
-i3 = Image.open('images/numbers/y0.5.png')
-iar3 = np.array(i3)
+    matchedAr = []
+    loadExamps = open('numArEx.txt', 'r').read()
+    loadExamps = loadExamps.split('\n')
 
-i4 = Image.open('images/sentdex.png')
-iar4 = np.array(i4)'''
+    i = Image.open(filePath)
+    iar = np.array(i)
+    iarl = iar.tolist()
 
-'''threshold(iar3)
-threshold(iar2)
-threshold(iar4)
+    inQuestion = str(iarl)
 
-fig = plt.figure()
-ax1 = plt.subplot2grid((8,6), (0,0), rowspan = 4, colspan = 3)
-ax2 = plt.subplot2grid((8,6), (4,0), rowspan = 4, colspan = 3)
-ax3 = plt.subplot2grid((8,6), (0,3), rowspan = 4, colspan = 3)
-ax4 = plt.subplot2grid((8,6), (4,3), rowspan = 4, colspan = 3)
+    for eachExample in loadExamps:
+        if len(eachExample) > 3:
+            splitEx = eachExample.split('::')
+            currentNum = splitEx[0]
+            currentAr = splitEx[1]
 
-ax1.imshow(iar)
-ax2.imshow(iar2)
-ax3.imshow(iar3)
-ax4.imshow(iar4)
+            eachPixEx = currentAr.split('],')
 
-plt.show()'''
+            eachPixInQ = inQuestion.split('],')
+
+            x = 0
+
+            while x < len(eachPixEx):
+                if eachPixEx[x] == eachPixInQ[x]:
+                    matchedAr.append(int(currentNum))
+
+                x += 1
+    print(matchedAr)
+    x = Counter(matchedAr)
+    print(x)
+
+whatNumIsThis('images/numbers/3.5.png')
